796e57f6174c971a4b822a2e28e6de5b6391fcfe
rohan-joby <rohanjobyak@gmail.com>
2021-04-29 21:36:39 +0530

    feat: add function to handle end of transition
    Refactored function to handle keypress and made it a separate function

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-document.addEventListener("keydown", function (e) {
+//to handle keyboard input
+function handleKeydown(e) {
   const key = document.querySelector(`.key[data-key = "${e.code}"]`);
-  console.log(e);
-  key.classList.toggle("playing");
-});
+  const audio = document.querySelector(`.sound[data-key = "${e.code}"]`);
+
+  //exit if any other button is pressed.
+  if (!audio) {
+    return;
+  }
+  audio.currentTime = 0;
+  audio.play();
+  //for animation
+  key.classList.add("playing");
+}
+
+//to handle end of transition
+function endTransition(e) {
+  if ((e.propertyName = "transform")) {
+    this.classList.remove("playing");
+  }
+}
+
+//listening for key press
+document.addEventListener("keydown", handleKeydown);
+
+//need to loop over NodeList to add Event Listener to listen for end of animation
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", endTransition));
