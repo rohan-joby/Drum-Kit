d5ec3a3516e044b762ef54810cdb87557f3f6c47
rohan-joby <rohanjobyak@gmail.com>
2021-05-01 00:57:36 +0530

    feat: made responsive

--- app.js
+++ app.js
@@ -3,22 +3,28 @@ function handleKeydown(e) {
   const key = document.querySelector(`.key[data-key = "${e.code}"]`);
   const audio = document.querySelector(`.sound[data-key = "${e.code}"]`);
 
-  if (!audio) {  //exit if any other button is pressed.
-    return;  
-  } 
+  if (!audio) {
+    //exit if any other button is pressed.
+    return;
+  }
   playSound(audio, key); //function to play drum kit sound
 }
 
 function handleClick(e) {
   const keyElement = e.target.closest(".key");
   const dataKey = keyElement.getAttribute("data-key");
-  
+
   const button = document.querySelector(`.key[data-key = ${dataKey}]`);
   const audio = document.querySelector(`.sound[data-key = "${dataKey}"]`);
 
-  if(!button){return;}
-  if (!audio) {return;} //exit if any other button is pressed.
+  if (!keyElement) {
+    return;
+  }
+  if (!audio) {
+    return;
+  } //exit if any other button is pressed.
   playSound(audio, button); //function to play drum kit sound
+  e.preventDefault();
 }
 
 function playSound(audio, key) {
@@ -28,7 +34,9 @@ function playSound(audio, key) {
 }
 
 document.addEventListener("keydown", handleKeydown); //listening for key press
+
 document.addEventListener("click", handleClick); //listening for button click
+document.addEventListener("touchend", handleClick, false); //for touchscreens, "click" event does not work on Safari if the element is not "clickable"
 
 //need to loop over NodeList to add Event Listener to listen for end of animation
 const keys = document.querySelectorAll(".key");
